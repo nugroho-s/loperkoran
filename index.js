b1494b7a0603542fc1fd4874f91530e3d7b25df8
Nugroho Satriyanto <nugroho.satriyanto@traveloka.com>
2019-12-14 12:28:09 +0700

    add pubsub function

--- index.js
+++ index.js
@@ -6,31 +6,34 @@ const NewsApi = new api(process.env.NEWSAPI)
 
 const blacklistedSources = ["Tribunnews.com"]
 
-const articles = await NewsApi.v2.topHeadlines({
-    country: 'id'
-}).then(news => news.articles)
-
-const embeds = articles.filter(function(article){
-        var isBlacklisted = false;
-        blacklistedSources.forEach(source => {
-            if (article.source.name.includes(source)){
-                isBlacklisted = true;
-            }
-        });
-        return !isBlacklisted;
-    }).map(article => ({
-        url: article.url,
-        thumbnail: {
-            width: 400,
-            height: 300,
-            url: article.urlToImage,
-        },
-        title: article.title,
-        description: article.description,
-        timestamp: article.publishedAt
-    })).slice(0,5);
-console.log(JSON.stringify(embeds));
-
-webhook.send("KORAN KORAN!\nAmbil ini, tambahlah ilmu pengetahuan",{
-    embeds: embeds
-})
+exports.sendNews = (event, context) => {
+    const articles = await NewsApi.v2.topHeadlines({
+        country: 'id'
+    }).then(news => news.articles)
+    
+    const embeds = articles.filter(function(article){
+            var isBlacklisted = false;
+            blacklistedSources.forEach(source => {
+                if (article.source.name.includes(source)){
+                    isBlacklisted = true;
+                }
+            });
+            return !isBlacklisted;
+        }).map(article => ({
+            url: article.url,
+            thumbnail: {
+                width: 400,
+                height: 300,
+                url: article.urlToImage,
+            },
+            title: article.title,
+            description: article.description,
+            timestamp: article.publishedAt
+        })).slice(0,5);
+    console.log(JSON.stringify(embeds));
+    
+    webhook.send("KORAN KORAN!\nAmbil ini, tambahlah ilmu pengetahuan",{
+        embeds: embeds
+    })
+    
+}

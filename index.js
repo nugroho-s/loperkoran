423f7e17e52137d9cccb37abe5e14e498617ddc4
Nugroho Satriyanto <nugroho.satriyanto@traveloka.com>
2019-12-14 13:01:27 +0700

    add pubsub and fix async news

--- index.js
+++ index.js
@@ -7,33 +7,32 @@ const NewsApi = new api(process.env.NEWSAPI)
 const blacklistedSources = ["Tribunnews.com"]
 
 exports.sendNews = (event, context) => {
-    const articles = await NewsApi.v2.topHeadlines({
+    NewsApi.v2.topHeadlines({
         country: 'id'
-    }).then(news => news.articles)
-    
-    const embeds = articles.filter(function(article){
-            var isBlacklisted = false;
-            blacklistedSources.forEach(source => {
-                if (article.source.name.includes(source)){
-                    isBlacklisted = true;
-                }
-            });
-            return !isBlacklisted;
-        }).map(article => ({
-            url: article.url,
-            thumbnail: {
-                width: 400,
-                height: 300,
-                url: article.urlToImage,
-            },
-            title: article.title,
-            description: article.description,
-            timestamp: article.publishedAt
-        })).slice(0,5);
-    console.log(JSON.stringify(embeds));
-    
-    webhook.send("KORAN KORAN!\nAmbil ini, tambahlah ilmu pengetahuan",{
-        embeds: embeds
-    })
-    
+    }).then(news => {
+        const embeds = news.articles.filter(function(article){
+                var isBlacklisted = false;
+                blacklistedSources.forEach(source => {
+                    if (article.source.name.includes(source)){
+                        isBlacklisted = true;
+                    }
+                });
+                return !isBlacklisted;
+            }).map(article => ({
+                url: article.url,
+                thumbnail: {
+                    width: 400,
+                    height: 300,
+                    url: article.urlToImage,
+                },
+                title: article.title,
+                description: article.description,
+                timestamp: article.publishedAt
+            })).slice(0,5);
+        console.log(JSON.stringify(embeds));
+        
+        webhook.send("KORAN KORAN!\nAmbil ini, tambahlah ilmu pengetahuan",{
+            embeds: embeds
+        })
+    });    
 }
